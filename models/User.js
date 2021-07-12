78c756f8d906e24fe6f9bdd10eb61083bdcb9873
Sanskrita Saha <sanskrita2001@gmail.com>
2021-07-12 21:18:33 +0530

    reset password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
@@ -28,7 +29,7 @@ const UserSchema = new mongoose.Schema({
 		select: false,
 	},
 	resetPasswordToken: String,
-	resetPasswordExpression: Date,
+	resetPasswordExpire: Date,
 	createdAt: {
 		type: Date,
 		default: Date.now,
@@ -36,20 +37,41 @@ const UserSchema = new mongoose.Schema({
 });
 
 //Encrypt password using bcryptjs
-UserSchema.pre('save',async function(next) {
-	const salt = await bcrypt.genSalt(10)
-	this.password = await bcrypt.hash(this.password,salt)
-})
+UserSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
 
 //Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
 	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
 		expiresIn: process.env.JWT_EXPIRE,
 	});
-}
+};
 
 //Match entered password and hashed password
 UserSchema.methods.matchPassword = async function (enteredPass) {
 	return await bcrypt.compare(enteredPass, this.password);
 };
+
+//Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+	//Generate token
+	const resetToken = crypto.randomBytes(20).toString('hex');
+
+	//Hash token and set to resetPasswordToken field
+	this.resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
+
+	//Set expire
+	this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+	return resetToken;
+};
+
 module.exports = mongoose.model('User', UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const {
+	register,
+	login,
+	getMe,
+	forgotPassword,
+} = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 //Add routes
-router.post('/register', register).post('/login', login).get('/me',protect, getMe);
+router
+	.post('/register', register)
+	.post('/login', login)
+	.get('/me', protect, getMe)
+	.post('/forgotpassword', forgotPassword);
 
 module.exports = router;
