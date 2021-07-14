ba5ccd4296741f3395031b0bb9d91c692ed17ee3
Sanskrita Saha <sanskrita2001@gmail.com>
2021-07-14 14:39:33 +0530

    Add reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getReviews, getReview } = require('../controllers/reviews');
+const { getReviews, getReview, addReview } = require('../controllers/reviews');
 const router = express.Router({ mergeParams: true });
 
 const Review = require('../models/Review');
@@ -8,13 +8,16 @@ const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 // Add routes
-router.route('/').get(
-	advancedResults(Review, {
-		path: 'bootcamp',
-		select: 'name description',
-	}),
-	getReviews
-);
+router
+	.route('/')
+	.get(
+		advancedResults(Review, {
+			path: 'bootcamp',
+			select: 'name description',
+		}),
+		getReviews
+	)
+	.post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id').get(getReview);
 
